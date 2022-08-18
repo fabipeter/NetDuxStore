9ce63978c1954ba42bea636b157910866cfbf5fd
fabipeter <53032909+fabipeter@users.noreply.github.com>
2022-08-18 22:18:38 +0100

    End of section 6

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import { history } from "../..";
 const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
@@ -61,9 +62,18 @@ const TestErrors = {
   getValidationError: () => requests.get("buggy/validation-error"),
 };
 
+const Basket = {
+  get: () => requests.get("basket"),
+  addItem: (productId: number, quantiy = 1) =>
+    requests.post(`basket?productId=${productId}&quantity=${quantiy}`, {}),
+  removeItem: (productId: number, quantiy = 1) =>
+    requests.delete(`basket?productId=${productId}&quantity=${quantiy}`),
+};
+
 const agent = {
   Catalog,
   TestErrors,
+  Basket
 };
 
 export default agent;
